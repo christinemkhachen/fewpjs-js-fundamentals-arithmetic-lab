914817242ccd63fd4c3fdf0204fa8eb9eac1f2c9
christinemkhachen <christinemkhachen@gmail.com>
2021-05-31 19:12:46 -0400

    commit tree 0be405e744000302eb4e64dd0120f2efbcfeb229

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let num1 = 62
-let num2 = 1
-let multiply = num1 * num2
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
 
 // let random = random integer greater than 0
 
 // capital M Math is a built-in object that comes with some cool functions
 // e.g. Math.random() is calling the built-in random function
-let random = Math.floor(Math.random() * 10) + 1 
-let num3 = 10
-let num4 = 6
-let mod = num3 % num4
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
 
-
-let max = Math.max(1, 2, 3, 4, 20)
+let max = Math.max(1, 2, 3, 4, 20);
+let num7 = 3 + 4;
